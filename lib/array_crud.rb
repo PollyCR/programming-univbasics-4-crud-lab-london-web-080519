fa82d3d684549764ecfcf8041a977d87aaefb62d
Polly Reynolds <polly.c.reynolds@gmail.com>
2019-06-11 16:07:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ array = []
 end
 
 def create_an_array
-array = [1,2]
+array = [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -15,15 +15,15 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+ array.pop 
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
